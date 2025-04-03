6acfe3eeb091324636d62ffeb92d9a262424f66c
diegodoshtml <gabrielvitorino551@gmail.com>
2025-04-03 20:36:06 -0300

    alteracao aula

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Button, Text, View } from "react-native";
 
 export default function HomeScreen() {
+    const [count, setCount] = useState(0);
 
     return (
         <View>
-            <Text>Um exemplo de texto!</Text>
+            <Text>Bem-vindo ao meu App!</Text>
+            <Text>Contador: {count}</Text>
+            <Button title="Aumentar"onPress={() => {}}/>
         </View>
     );
 }
