c1b93cd1f83737917b0243277760b1ebdf84a9c4
diegodoshtml <gabrielvitorino551@gmail.com>
2025-04-03 22:01:04 -0300

    Aula 03-04

--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,30 @@ import React, { useState } from "react";
 import { Button, Text, View } from "react-native";
 
 export default function HomeScreen() {
-    const [count, setCount] = useState(0);
+    const STARTVALUE = 0;
+    const [count, setCount] = useState(STARTVALUE);
+
+    function alterarCount(value: number){
+        setCount(count + value);
+    }
 
     return (
         <View>
             <Text>Bem-vindo ao meu App!</Text>
             <Text>Contador: {count}</Text>
-            <Button title="Aumentar"onPress={() => {}}/>
+            <Button 
+                title="Aumentar"
+                onPress={() => {setCount(count + 1)}} color="green"/>
+                <br></br>
+            <Button 
+                title="Diminui"
+                onPress={() => {setCount(count - 1)}} color="red"/>
+                <br></br>
+            <Button 
+                title="Resetar"
+                onPress={() => {setCount(0)}} color="blue"/>
+                <br></br>
+
         </View>
     );
 }
